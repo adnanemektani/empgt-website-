31f44d9654ec56c9b9ab1d664b54f9308ac6674f
Adnane MEKTANI <adnanemektani02@gmail.com>
2026-08-17 17:40:33 +0100

    ...

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -9,7 +9,7 @@ export const sanityClient = createClient({
   projectId: SANITY_PROJECT_ID,
   dataset: SANITY_DATASET,
   apiVersion: "2024-01-01",
-  useCdn: true,
+  useCdn: false,
 });
 
 export type SanityNews = {
